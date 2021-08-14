60ae222ef0649d0bcc5527f2cf9edbc9e54ad388
Jacob Bathan <jacobbathan@gmail.com>
2021-08-14 22:57:53 +0000

    Delete target directory

--- target/debug/deps/snappy-fd8eba493c3a70b6.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/PersonalProjects/rust/snappy/target/debug/deps/snappy-fd8eba493c3a70b6: src/main.rs
-
-/mnt/c/PersonalProjects/rust/snappy/target/debug/deps/snappy-fd8eba493c3a70b6.d: src/main.rs
-
-src/main.rs:
